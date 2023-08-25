83d453124c266968210facfb2249d4651dec2d74
Bernardo Stearns <bernardostearns@DSIS-MBP-2.station>
2023-08-25 06:59:35 +0100

    init

--- mlm_ukplab/MLM/train_mlm.py
+++ mlm_ukplab/MLM/train_mlm.py
@@ -18,15 +18,24 @@ import os
 from dataloader import TokenizedSentencesDataset
 
 
+def decide_trainedModelsDir():
+    lastEpochDir_files = os.listdir("/content/drive/MyDrive/phd/code/data/run_20230821/output_1epoch/")
+    currEpochDir_files = os.listdir("/content/drive/MyDrive/phd/code/data/run_20230821/output_2epoch/") 
+    if len(currEpochDir_files) == 0:
+        return lastEpochDir_files  
+    else:
+        return currEpochDir_files
+
+epoch_num = 2
 per_device_train_batch_size = 64
-trained_models_dir = os.listdir("/content/drive/MyDrive/phd/code/data/run_20230821/output_1epoch/")
+trained_models_dir = decide_trainedModelsDir 
 last_batch_idx, model_foldername = max([(int(folder.split("-")[1]),folder) for folder in trained_models_dir]) if len(trained_models_dir) > 0 else (0, None)
 curr_batch_idx = last_batch_idx + 1
 print(f"last_batch_idx : {last_batch_idx}")
 print(f"curr_batch_idx : {curr_batch_idx}")
 model_name = "bert-base-uncased"
 log_folder  = f"/content/drive/MyDrive/phd/code/data/run_20230821/logs/"
-model_folder = f"/content/drive/MyDrive/phd/code/data/run_20230821/output_1epoch/{model_foldername}" if model_foldername else None
+checkpointModel_folder = f"{trained_models_dir}/{model_foldername}" if model_foldername else None
 batches_folder = "/content/drive/MyDrive/phd/code/data/run_20230821/batches_gt5_20230822/"
 train_filepath = os.path.join(batches_folder, f"batch_{curr_batch_idx}.txt") 
 save_steps = 1000               #Save model every 1k steps
@@ -35,12 +44,12 @@ use_fp16 = False                #Set to True, if your GPU supports FP16 operatio
 max_length = 100                #Max length for a text input
 do_whole_word_mask = True       #If set to true, whole words are masked
 mlm_prob = 0.15                 #Probability that a word is replaced by a [MASK] token
-output_dir = "/content/drive/MyDrive/phd/code/data/run_20230821/output_1epoch/batch-{}-{}-{}".format(curr_batch_idx, model_name,  datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+output_dir = "/content/drive/MyDrive/phd/code/data/run_20230821/output_{}epoch/batch-{}-{}-{}".format(epoch_num, curr_batch_idx, model_name,  datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 print("Save checkpoints to:", output_dir)
 
 # Load the model
-model = AutoModelForMaskedLM.from_pretrained(model_folder if model_folder != None else "bert-base-uncased")
-tokenizer = AutoTokenizer.from_pretrained(model_folder if model_folder != None else "bert-base-uncased")
+model = AutoModelForMaskedLM.from_pretrained(checkpointModel_folder if checkpointModel_folder != None else "bert-base-uncased")
+tokenizer = AutoTokenizer.from_pretrained(checkpointModel_folder if checkpointModel_folder != None else "bert-base-uncased")
 
 
 
